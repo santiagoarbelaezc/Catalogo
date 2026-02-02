10d4fce6e43f0862eba3a58087989c63af4b8e7f
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-02-02 14:19:37 -0500

    subiendo nuevas modifaciones, adicion del navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isVisible = true;
 
+  @HostListener('window:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    this.isVisible = event.clientY < 120; // Show when mouse is within 120px from top
+  }
 }
