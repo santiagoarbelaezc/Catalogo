882e751cef5b8ff4738a35c5ad67ca9c66064145
Santiago Arbelaez Contreras <santiago.arbelaezc@uqvirtual.edu.co>
2026-03-31 23:47:53 -0500

    paginacion agregada

--- src/app/pages/districol/districol.component.ts
+++ src/app/pages/districol/districol.component.ts
@@ -15,8 +15,34 @@ import { NavbarComponent } from "../../components/shared/navbar/navbar.component
 })
 export class DistricolComponent implements OnInit {
   products: CatalogProduct[] = [];
-
   
+  // Paginación
+  currentPage: number = 1;
+  itemsPerPage: number = 12;
+
+  get paginatedProducts(): CatalogProduct[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.products.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.products.length / this.itemsPerPage);
+  }
+
+  get pagesList(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  changePage(page: number): void {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      // Hacer scroll suave hacia el inicio de la sección de productos
+      const element = document.getElementById('districol-section');
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }
+  }
 
   constructor(private productsService: ProductsService) {}
   
